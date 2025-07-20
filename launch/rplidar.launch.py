0f851068a65d815aafc443e58b5f246ba0be63f9
Helltaker1234 <ukendo3333@gmail.com>
2025-07-21 06:01:44 +0900

    [20250721 - 4강 강의; 라이다 ROS로 올려보기] - rplidar a1 직접 이 패키지로 돌려봐야 함.

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,23 @@
+# 이 사람도 rplidar a1 을 씀.
+# 시리얼 포트 정도만 맞게 수정하면 될듯.
+
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
